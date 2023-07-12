ddd288a7316d277aca923724ac00408c7da9381c
«Ales» <alopusansky@gmail.com>
2023-07-12 11:44:26 +0300

    add solution in task_004

---
+++ task_003/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[row, col];
+    Random random = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(leftRange, rightRange);
+        }
+    }
+    return matrix;
+}
+
+int ReadInt(string messege)
+{
+    System.Console.Write(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write(arr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMartrix.GetLength(1); k++)
+            {
+                sum += firstMartrix[i, k] * secondMartrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+}
+
+int row = ReadInt("Введите количество столбцов 1 матрицы: ");
+int col = ReadInt("Введите количество колонок 1 матрицы и столбцов 2-й: ");
+
+int col2 = ReadInt("Введите количество колонок 2 матрицы: ");
+
+int[,] firstMatrix = FillMatrix(row, col, 0, 10);
+int[,] secondMatrix = FillMatrix(col, col2, 0, 10);
+
+int [,]resultMatrix = new int [row, col2];
+
+System.Console.WriteLine("first matrix:");
+PrintMatrix(firstMatrix);
+
+System.Console.WriteLine("second matrix:");
+PrintMatrix(secondMatrix);
+
+MultiplyMatrix(firstMatrix, secondMatrix, resultMatrix);
+System.Console.WriteLine("result matrix:");
+PrintMatrix(resultMatrix);
---
+++ task_004/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из двузначных чисел. Напишите программу, которая будет построчно выводить массив,
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+void PrintArray3D(int[,,] array3D)
+{
+    for (int i = 0; i < array3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Random rand = new Random();
+                array3D[i, j, k] = rand.Next(10, 100);
+                System.Console.Write(array3D[i, j, k] + $"({j}, {k}, {i})");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
+
+int[,,] array3D = new int[2, 2, 2];
+PrintArray3D(array3D);
